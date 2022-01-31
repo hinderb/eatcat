dc8fbdb60e53249de652d26ff1429ff3f8d1de96
hinderb <98684809+hinderb@users.noreply.github.com>
2022-01-31 15:06:33 +0700

    Update index.js

--- static/index.js
+++ static/index.js
@@ -13,7 +13,7 @@ let hide = false;
 let __Time = 20;
 let __k = 4;
 let _close = false;
-var url = 'https://raw.githubusercontent.com/hinderb/eatkeqingvers/main/static/image/ClickBefore.png';
+var url = 'https://hinderb.github.io/eatkeqingvers/static/image/ClickBefore.png';
 
 function isplaying() {
     return document.getElementById('welcome').style.display == 'none' &&
